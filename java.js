41749f56e3ae93d2b4ff63703c1eaa5022fa81d8
DylanMacres <Dylan.Macres@du.edu>
2022-07-26 19:00:00 -0600

    fixed id selectors for hours of the day in military time, tested one hour on quereySelecting

--- java.js
+++ java.js
@@ -2,3 +2,17 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").text(todayDate);
 
+const currentHour = moment().hour();
+console.log(moment().hour())
+
+var hour9am = document.querySelector("#hour9")
+if (currentHour === 9){
+    hour9am.setAttribute("style","background color: red");
+
+} else if (currentHour < 9){
+    hour9am.setAttribute("style","background-color: green")
+
+}else {
+    hour9am.setAttribute("style", "background-color: grey")
+}
+
