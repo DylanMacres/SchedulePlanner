6ee5932abf0f69fc585658351e819c6282875a7c
DylanMacres <Dylan.Macres@du.edu>
2022-07-30 17:19:38 -0600

    updated readme and try at presistent data

--- java.js
+++ java.js
@@ -1,10 +1,19 @@
+
+//set the current day and hour and make a variable for 
 var todayDate = moment().format("dddd, MMM Do YYYY");
 $("#currentDay").text(todayDate);
 
 const currentHour = moment().hour();
 console.log(moment().hour());
 
-var hour7am = document.querySelector("#hour7");
+//set the save button class as a variable 
+var saveButton = $(".saveBtn")
+
+
+
+
+//Name the different hours as variables
+const hour7am = document.querySelector("#hour7");
 var hour8am = document.querySelector("#hour8");
 var hour9am = document.querySelector("#hour9");
 var hour10am = document.querySelector("#hour10");
@@ -17,7 +26,10 @@ var hour4pm = document.querySelector("#hour16");
 var hour5pm = document.querySelector("#hour17");
 
 
-if (currentHour === 7) {
+ //make variables that change when the hour changes using the moment library and repeat for all hours
+
+
+ if (currentHour === 7) {
     hour7am.setAttribute("style", "background-color: red");
 } else if (currentHour < 7) {
     hour7am.setAttribute("style", "background-color: green");
@@ -27,7 +39,7 @@ if (currentHour === 7) {
 
 
 if (currentHour === 8) {
-    hour8am.setAttribute("style", "background color: red");
+    hour8am.setAttribute("style", "background-color: red");
 } else if (currentHour < 8) {
     hour8am.setAttribute("style", "background-color: green");
 } else {
@@ -37,7 +49,7 @@ if (currentHour === 8) {
 
 
 if (currentHour === 9) {
-    hour9am.setAttribute("style", "background color: red");
+    hour9am.setAttribute("style", "background-color: red");
 } else if (currentHour < 9) {
     hour9am.setAttribute("style", "background-color: green");
 } else {
@@ -46,7 +58,7 @@ if (currentHour === 9) {
 
 
 if (currentHour === 10) {
-    hour10am.setAttribute("style", "background color: red");
+    hour10am.setAttribute("style", "background-color: red");
 } else if (currentHour < 10) {
     hour10am.setAttribute("style", "background-color: green");
 } else {
@@ -55,7 +67,7 @@ if (currentHour === 10) {
 
 
 if (currentHour === 11) {
-    hour11am.setAttribute("style", "background color: red");
+    hour11am.setAttribute("style", "background-color: red");
 } else if (currentHour < 11) {
     hour11am.setAttribute("style", "background-color: green");
 } else {
@@ -64,7 +76,7 @@ if (currentHour === 11) {
 
 
 if (currentHour === 12) {
-    hour12pm.setAttribute("style", "background color: red");
+    hour12pm.setAttribute("style", "background-color: red");
 } else if (currentHour < 12) {
     hour12pm.setAttribute("style", "background-color: green");
 } else {
@@ -73,7 +85,7 @@ if (currentHour === 12) {
 
 
 if (currentHour === 13) {
-    hour1pm.setAttribute("style", "background color: red");
+    hour1pm.setAttribute("style", "background-color: red");
 } else if (currentHour < 13) {
     hour1pm.setAttribute("style", "background-color: green");
 } else {
@@ -82,7 +94,7 @@ if (currentHour === 13) {
 
 
 if (currentHour === 14) {
-    hour2pm.setAttribute("style", "background color: red");
+    hour2pm.setAttribute("style", "background-color: red");
 } else if (currentHour < 14) {
     hour2pm.setAttribute("style", "background-color: green");
 } else {
@@ -92,7 +104,7 @@ if (currentHour === 14) {
 
 
 if (currentHour === 15) {
-  hour3pm.setAttribute("style", "background color: red");
+  hour3pm.setAttribute("style", "background-color: red");
 } else if (currentHour < 15) {
     hour3pm.setAttribute("style", "background-color: green");
 } else {
@@ -102,7 +114,7 @@ if (currentHour === 15) {
 
 
 if (currentHour === 16) {
-    hour4pm.setAttribute("style", "background color: red");
+    hour4pm.setAttribute("style", "background-color: red");
 } else if (currentHour < 16) {
     hour4pm.setAttribute("style", "background-color: green");
 } else {
@@ -111,7 +123,7 @@ if (currentHour === 16) {
 
 
 if (currentHour === 17){
-    hour5pm.setAttribute("style","background color: red");
+    hour5pm.setAttribute("style","background-color: red");
 
 } else if (currentHour < 17){
     hour5pm.setAttribute("style","background-color: green")
@@ -119,3 +131,37 @@ if (currentHour === 17){
 }else {
     hour5pm.setAttribute("style", "background-color: grey")
 }
+
+//recieve the data from the localstorage to presist
+function getData(){
+    $("#hour7").text(localStorage.getItem("hour7am"))
+    $("#hour8").text(localStorage.getItem("hour8am"))
+    $("#hour9").text(localStorage.getItem("hour9am"))
+    $("#hour10").text(localStorage.getItem("hour10am"))
+    $("#hour11").text(localStorage.getItem("hour11am"))
+    $("#hour12").text(localStorage.getItem("hour12pm"))
+    $("#hour13").text(localStorage.getItem("hour1pm"))
+    $("#hour14").text(localStorage.getItem("hour2pm"))
+    $("#hour15").text(localStorage.getItem("hour3pm"))
+    $("#hour16").text(localStorage.getItem("hour4pm"))
+    $("#hour17").text(localStorage.getItem("hour5pm"))
+
+};
+
+getData();
+
+//have the save button save the data
+$(saveButton).on("click", function(event){
+   var userInput = $(this).siblings(".form-control").val().trim();
+    console.log(userInput);
+    var time = $(this).parent().attr("id");
+    console.log(time)
+    localStorage.setItem(time, userInput);
+    localStorage.getItem(time, userInput)
+    getData();
+})
+
+
+
+
+
